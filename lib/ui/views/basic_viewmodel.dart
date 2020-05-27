1f02c575f9dd4781873cf2bdbfc773f5587f03ce
shawn <shawn@wiredcraft.com>
2020-05-27 12:38:35 +0800

    update deprecated method

--- lib/ui/views/basic_viewmodel.dart
+++ lib/ui/views/basic_viewmodel.dart
@@ -3,7 +3,6 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class BasicViewModel extends BaseViewModel {
-//  final AppConfig config = locator<AppConfig>();
   final DialogService _dialogService = locator<DialogService>();
   final NavigationService navigationService = locator<NavigationService>();
 
